6d0b56e71e0dcf3607f664730886ac5e9977c575
Baz <bk@thinkloop.com>
2016-02-03 09:27:24 +0000

    simplified tests

---
+++ tests/select.js
@@ -0,0 +1,18 @@
+var Similar = require('../src/similar');
+
+describe("map may or may not exist", () => {
+    var MapOrSimilar = require('../src/map-or-similar'),
+        mapOrSimilar = new MapOrSimilar();
+
+    it("chose Map because it exists", () => { expect(mapOrSimilar instanceof Map).toEqual(true); });
+    it("was forced to similar", () => { expect(mapOrSimilar instanceof Similar).toEqual(false); });
+});
+
+describe("force similar", () => {
+    process.env.TEST_MAPORSIMILAR = true;
+    var MapOrSimilar = require('../src/similar'),
+        mapOrSimilar = new MapOrSimilar();
+
+    it("was forced to similar", () => { expect(mapOrSimilar instanceof Similar).toEqual(true); });
+    it("was forced to similar", () => { expect(mapOrSimilar instanceof Map).toEqual(false); });
+});
